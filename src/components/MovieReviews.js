7a52430daa4a8fd6ec19562fd5b5ae0b2c17b7cd
bellaappel <bellaappel0@gmail.com>
2021-05-19 13:49:10 -0400

    commit tree 4def505b29b0ebf6f241ca4c8aff240d223a632a

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
+import React from "react";
 // Code MovieReviews Here
+
+const MovieReviews = (props)=> (
+  <div className="review-list">
+    {props.reviews.map((reviewData, index)=> {
+      const { headline, link, multimedia, summary_short } = reviewData
+      return(
+        <div className="review" key={index.toString()}>
+          <h4><a href={link.url}>{headline}</a></h4>
+          <img src={multimedia.src} alt="face" width={multimedia.width} height={multimedia.height}/>
+          <p>{summary_short}</p>
+        </div>
+      )
+    })}
+  </div>
+)
+export default MovieReviews
